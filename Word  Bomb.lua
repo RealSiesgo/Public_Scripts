c8b4fe25de8196b63ed5d57a78f11ed1fdc0dc9c
RealSiesgo <93088981+RealSiesgo@users.noreply.github.com>
2022-06-11 11:51:30 -0300

    hehe

--- Word  Bomb.lua
+++ Word  Bomb.lua
@@ -1,57 +1,67 @@
---WARNING: YOU MUST EXECUTE EVERYTIME ITS YOUR TURN
-string_len_max=5
---the script may increase max length 1 by 1 if theres no words for the hint matching the max length
+-- /*WARNING*\: THIS SCRIPT GOES ON UNTIL THE CURRENT ROUND ENDS OR THE WINDOW SCALE CHANGES
+-- and im too lazy to fix that :D
 
-math.randomseed(os.time())
+-- \Settings/ --
 
-local MATH_RANDOM_MIN,MATH_RANDOM_MAX=5,25
+string_len_max=5 --> the script may increase max length 1 by 1 if there are no words for the hint matching the max length
+
+local MATH_RANDOM_MIN,MATH_RANDOM_MAX=5,17
 
 -- /Settings\ --
-warn("MADE BY Siesgo#8832")
-print("MADE BY Siesgo#8832")
--- -- -- -- --
 
+math.randomseed(os.time())
+alreadyguessedtable={}
 loadstring(game:HttpGet("https://raw.githubusercontent.com/angeld23/raw-text-host/master/condensed_english_words"))()
-table.insert(ENGLISH_WORDS,"arab")
-table.insert(ENGLISH_WORDS,"nigger") --game accepts n word ¯\_(ツ)_/¯
 table.insert(ENGLISH_WORDS,"pes")table.insert(ENGLISH_WORDS,"des")
-
-HINT=""
-tfrm=game:GetService("Players").LocalPlayer.PlayerGui.GameUI.Container.GameSpace.DefaultUI.GameContainer.DesktopContainer.InfoFrameContainer.InfoFrame.TextFrame
-TEXTBOX=game:GetService("Players").LocalPlayer.PlayerGui.GameUI.Container.GameSpace.DefaultUI.GameContainer.DesktopContainer.Typebar.Typebox
-for i,v in next,tfrm:GetChildren() do
-    --FIND HINTS
-    if v.Name=="LetterFrame" and v.Visible then
-        --IF HINTLETTERFRAME AND HINTLETTERFRAME VISIBLE THEN
-        HINT=HINT..v.Letter.TextLabel.Text
-        --GET THE HINT MSG
+while task.wait() do
+    center=game:GetService("Players").LocalPlayer.PlayerGui.GameUI.Container.GameSpace.DefaultUI.GameContainer.DesktopContainer.Typebar.Center
+    if center.BackgroundTransparency==1 then
+        repeat
+            center:GetPropertyChangedSignal("BackgroundTransparency"):Wait()
+            wait()
+        until center.BackgroundTransparency~=1
     end
-end
+    HINT=""
+    tfrm=game:GetService("Players").LocalPlayer.PlayerGui.GameUI.Container.GameSpace.DefaultUI.GameContainer.DesktopContainer.InfoFrameContainer.InfoFrame.TextFrame
+    TEXTBOX=game:GetService("Players").LocalPlayer.PlayerGui.GameUI.Container.GameSpace.DefaultUI.GameContainer.DesktopContainer.Typebar.Typebox
 
-if alreadyguessedtable==nil then
-    getgenv().alreadyguessedtable={}
-end
-
-function trytofind(length)
-    for i,v in next, ENGLISH_WORDS do
-        if TEXTBOX.Visible==false then break end
-        if v:lower():match(HINT:lower()) and string.len(v)<length and (not table.find(alreadyguessedtable,v)) then
-            table.insert(alreadyguessedtable,v)
-            for i=1,v:len() do
-                wait(math.random(MATH_RANDOM_MIN,MATH_RANDOM_MAX)*10^-2)
-                TEXTBOX.Text=v:sub(1,i)
-            end
-            wait(.25)
-            firesignal(TEXTBOX.FocusLost, {true})
-            FOUNDTEXT=true
-            break
-        end
+    if TEXTBOX.Visible==false then
+        repeat
+            task.wait()
+        until TEXTBOX.Visible
     end
     
-    return (FOUNDTEXT or false)
-end
-if trytofind(string_len_max)==false then
-    for i=string_len_max,30 do
-        if trytofind(i) then break end
+    for i,v in next,tfrm:GetChildren() do
+        --FIND HINTS
+        if v.Name=="LetterFrame" and v.Visible then
+            --IF HINTLETTERFRAME AND HINTLETTERFRAME VISIBLE THEN
+            HINT=HINT..v.Letter.TextLabel.Text
+            --GET THE HINT MSG
+        end
     end
+    --wait(1.5)
+    function trytofind(length)
+        for i,v in next, ENGLISH_WORDS do
+            if v:lower():match(HINT:lower()) and string.len(v)<length and (not table.find(alreadyguessedtable,v)) then
+                table.insert(alreadyguessedtable,v)
+                for i=1,v:len() do
+                    --wait(math.random(MATH_RANDOM_MIN,MATH_RANDOM_MAX)*10^-2)
+                    TEXTBOX.Text=v:sub(1,i)
+                end
+                task.wait()
+                oldtb=game:GetService("UserInputService"):GetFocusedTextBox()
+                firesignal(TEXTBOX.FocusLost, true)
+                if oldtb then oldtb:CaptureFocus() end
+                return true
+            end
+        end
+        
+        return false
+    end
+    if trytofind(string_len_max)==false then
+        for i=string_len_max,30 do
+            if trytofind(i) then break end
+        end
+    end
+    wait(.75)
 end
