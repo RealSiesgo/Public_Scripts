bb6ca9607939c976e61472cd87f5679e43ab5a9a
RealSiesgo <93088981+RealSiesgo@users.noreply.github.com>
2022-06-25 00:39:03 -0300

    testing

--- Anti_Bad_Words.lua
+++ Anti_Bad_Words.lua
@@ -1,5 +1,5 @@
 --wrote this script in case yall get banned everytime for saying bad words like me too lmao
-words={"fuck","nigger","faggot","nier","niger","suck my","retard","retarded","nigga","bitch","hoe","cock","dick","fuvk"}
+words={"fu.k","n.gger","faggot","nier","niger","suck my","retard","blowjob","bitch","hoe","cock","dick","osama","shit","anal","rape","fack","daddy","f.ck","damn"}
 repeat wait(.5) until game:IsLoaded()
 if (not game.ReplicatedStorage:FindFirstChild("DefaultChatSystemChatEvents")) or (not game.ReplicatedStorage.DefaultChatSystemChatEvents:FindFirstChild("SayMessageRequest")) then
 	warn("This game doesnt have roblox's chatting System.")
@@ -15,12 +15,18 @@ function _f(txt)
 	end
 	return false
 end
+function tagtext(text)
+	local TAGTEXT=""
+	text=text:len()
+	for i=1,text do TAGTEXT=TAGTEXT.."#"end
+	return TAGTEXT
+end
 hm=hookmetamethod(game,"__namecall",function(Self,...)
 	local args={...}
 	local ncm = getnamecallmethod()
 	if Self==game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest and ncm:lower()=="fireserver" and _f(args[1]) then
 		local text=args[1]
-		for i,v in next, words do text=text:gsub(v,"###") end
+		for i,v in next, words do text=text:gsub(v,tagtext) end
 		return Self:FireServer(text,args[2])
 	end
 
