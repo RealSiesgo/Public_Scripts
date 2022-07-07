ed44a241573b23b0c3bd65dac821d9adb74a987d
RealSiesgo <93088981+RealSiesgo@users.noreply.github.com>
2022-07-07 15:59:25 -0300

    Update Anti_Chat_Hax.lua

--- Anti_Chat_Hax.lua
+++ Anti_Chat_Hax.lua
@@ -1,20 +1,57 @@
+--[[
+getgenv().SETTINGS={
+	["Notifications_Enabled"]=true
+	["Notifications_Duration"]=8
+}
+loadstring(game:HttpGet(""))()
+]]
 repeat wait() until game:IsLoaded()
+local chatbx = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui",9e9).Chat.Frame.ChatBarParentFrame.Frame.BoxFrame.Frame.ChatBar
 local blacklisted = {}
-local chatbx = game:GetService("Players").LocalPlayer.PlayerGui.Chat.Frame.ChatBarParentFrame.Frame.BoxFrame.Frame.ChatBar
+local notify_me = false
+local duration = 8
+if SETTINGS then
+	if SETTINGS["Notifications_Enabled"] then
+		notify_me = true
+	end
+	if SETTINGS["Notifications_Duration"] then
+		duration = SETTINGS["Notifications_Duration"]
+	end
+end
 
+warn("MADE BY Siesgo#8832")
+
+function notify(txt)
+	if not notify_me then return end
+	game:GetService("StarterGui"):SetCore("SendNotification", {
+		Title = "Anti-ChatHax (BLOCKED MSG)";
+		Text = txt;
+		Duration = 7;
+	})
+end
 mehook1=hookmetamethod(game:GetService("Players"),"__namecall",function(...)
 	local Self=...
-	if not checkcaller() and Self==chatbx and getnamecallmethod()=="CaptureFocus" then
-		table.insert(blacklisted,getcallingscript())
+	local ncm=getnamecallmethod()
+	local scr=getcallingscript()
+	if not checkcaller() and Self==chatbx and scr.Parent.Name~="PlayerScripts"and scr.Name~="ChatScript"and #scr:GetDescendants()~=16 then
+		if ncm=="CaptureFocus" then
+			table.insert(blacklisted,scr)
+			--print("stopped",scr,"from capturing focus")
+			return
+		elseif ncm=="ReleaseFocus" then
+			--> Extra protection maybe??
+			--print("stopped",scr,"from releasing focus")
+			return
+		end
 	end
 	return mehook1(...)
 end)
-mehook2=hookmetamethod(game:GetService("Players"),"__newindex",function(...)
-	local Self=...
-	if not checkcaller() and Self==chatbx and table.find(blacklisted,getcallingscript()) then
-		print("Blocked ChatHax.")
-		chatbx:ReleaseFocus()
-		return error("Chat hax script stopped.")
+mehook2=hookmetamethod(game:GetService("Players"),"__newindex",newcclosure(function(...)
+	local Self,property,msg=...
+	if not checkcaller() and table.find(blacklisted,getcallingscript()) and property=="Text" and typeof(msg)=="string" and Self==chatbx then
+		--print("Blocked ChatHax.")
+		notify(msg)
+		return
 	end
 	return mehook2(...)
-end)
+end))
